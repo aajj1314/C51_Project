c09f8f9abe6b13ae22e720a45e4382143ea5f675
aajj1314 <3207294764@qq.com>
2025-12-20 11:00:04 +0000

    "13"

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include<8051.h>
+void Delay(void)
+{
 
+
+
+}
 void main()
 {
     P0 = 0x55;
     P1 = 0x0f;
     P3 =0x01;
     TMOD = 0x01 ;
-    for (;;){}
+
+    Delay();
+    P2=0xf0 ;
     while(1);
+
 
 }
